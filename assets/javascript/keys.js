340711f55dd37ed0b0e967f31e1d047374a5c8ef
ewill20 <ewill2020@gmail.com>
2017-08-25 21:43:24 -0500

    initial commit

---
+++ assets/javascript/keys.js
@@ -0,0 +1,12 @@
+console.log('this is loaded');
+exports.twitterkeys = {
+    consumer_key: '5OLBsWMTMhmh1Oj3aIUKlfkkF',
+    consumer_secret: 'SoR6cP9mlKey39vjWVt5sSnplBIi05j7JxxTtqdTI3tLHtQchB',
+    access_token_key: '539295545-JextoeZDLMRTHi59tukElW7GPaOCaK3PHpalp9St',
+    access_token_secret: 'Tq7X6XuhVxnp79WyhUcbFdY2FRVAd03i9DlC91Y9znEbE',
+};
+console.log('this is spotify');
+exports.spotifykeys = {
+    consumer_key: '82d3691d3b25410a98578af128ccdd73',
+    consumer_secret: 'aadae6add1a847929ec9b24ee67763ad',
+}
