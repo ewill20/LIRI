b8864703eb2863cb2a031527c558ad6b235debb6
ewill20 <ewill2020@gmail.com>
2017-08-28 12:45:07 -0500

    done?

--- assets/javascript/liri.js
+++ assets/javascript/liri.js
@@ -91,9 +91,61 @@ for (var i = 3; i>nodeArgv.length; i++) {
                 } else {
                     console.log("An Error Occurred");
                 }
-            })
+            });
+        }
+
+        function runOMDB(movie) {
+            var omdbURL = "http//www.omdbapi.com/?t=" + movie + "&plot=short&tomatoes=true";
+
+            request(omdbURL, function(error, response, body) {
+                if(!error && response.statusCode == 200) {
+                    var body = JSON.parse(body);
+
+                    console.log("Title: " + body.Title);
+                    console.log("Release Year: " + body.Year);
+                    console.log("IMdB Rating: " + body.imdbRating);
+                    console.log("Country: " + body.Country);
+                    console.log("Language: " + body.Language);
+                    console.log("Plot: " + body.Plot);
+                    console.log("Actors: " + body.Actors);
+                    console.log("Rotten Tomatoes Rating: " + body.tomatoRating);
+                    console.log("Rotten Tomatoes URL: " + body.tomatoURL);
+
+
+                // Adds information to log.txt //
+                fs.appendFile('log.txt', "Title: " + body.Title);
+                fs.appendFile('log.txt', "Release Year: " + body.Year);
+                fs.appendFile('log.txt', "IMdB Rating: " + body.imdbRating);
+                fs.appendFile('log.txt', "Country: " + body.Country);
+                fs.appendFile('log.txt', "Language: " + body.Language);
+                fs.appendFile('log.txt', "Plot: " + body.Plot);
+                fs.appendFile('log.txt', "Actors: " + body.Actors);
+                fs.appendFile('log.txt', "Rotten Tomatoes Rating: " + body.tomatoRating);
+                fs.appendFile('log.txt', "Rotten Tomatoes URL: " + body.tomatoURL);
+                
+                } else {
+                    console.log("An Error Occurred");
+                }
+                if (movie === "Mr. Nobody") {
+                    console.log("-----------------");
+                    console.log("If you haven't watched 'Mr. Nobody' then you should: http://www.imdb.com/title/tt0485947/");
+                    console.log("It can also be found on Netflix!");
+
+                    // Adds information to log.txt //
+                    fs.appendFile('log.txt', "-----------------");
+                    fs.appendFile('log.txt', "If you haven't watched 'Mr. Nobody' then you should: http://www.imdb.com/title/tt0485947/");
+                    fs.appendFile('log.txt', "It can also be found on Netflix!");
+                }
+            });
         }
              
+        function followCommand() {
+            fs.readFile('random.txt', "utf8", function(error, data) {
+                var txt = data.split(',')
+
+                spotifySong(txt[1]);
+            });
+        }
 
 
 
