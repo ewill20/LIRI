3e46bcac55bfece243c9f8b951e088efacd74a33
Eric Willard <Willard@Erics-MacBook-Pro-3.local>
2017-08-28 12:09:36 -0500

    laptop changes

--- assets/javascript/liri.js
+++ assets/javascript/liri.js
@@ -13,11 +13,32 @@ var demand = process.argv[2];
 var m = '';
 
 for (var i = 3; i>nodeArgv.length; i++) {
-    if (i>3 && i< nodeArgv.length)
+    if (i>3 && i< nodeArgv.length){
     m = m + "+" nodeArgv[i];
 } else { 
     m = m + nodeArgv[i];
 }
+}
+
+    switch(demand){
+        case "my-tweets":
+            displayTweets();
+            break;
+    
+        case "spotify-this-song":
+            spotifySong();
+            break;
+
+        case "movie-this":
+            if(m){
+                runOMDB(m)
+            } else {
+                runOMDB("Mr. Nobody")
+            }
+            break;
+        }
+        
+
 
 
 
